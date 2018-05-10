077c5c3ff4ded62b7de86e0c45fafb6c49a88968
qaraluch <prezenciq@gmail.com>
2018-05-10 14:35:04 +0200

    Add examples

---
+++ examples/run.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const util = require("util");
+const { walk, walkProcessed } = require("../index.js");
+
+const writeFile = util.promisify(fs.writeFile);
+
+const mockupPath = "./test/fixtures";
+
+(async () => {
+  try {
+    //Execute:
+    const [walkOutput, walkProcessedOutput] = await Promise.all([
+      walk({ path: mockupPath }),
+      walkProcessed({
+        path: mockupPath
+      })
+    ]);
+
+    //Saves:
+    const save__walk = writeFile(
+      "./examples/walk.json",
+      JSON.stringify(walkOutput, null, 2)
+    );
+
+    const save__walkItem = writeFile(
+      "./examples/walk-item.json",
+      JSON.stringify(walkOutput[2], null, 2)
+    );
+
+    const save__walkProcessed = writeFile(
+      "./examples/walkProcessed.json",
+      JSON.stringify(walkProcessedOutput, null, 2)
+    );
+
+    const save__walkProcessedItem = writeFile(
+      "./examples/walkProcessed-item.json",
+      JSON.stringify(walkProcessedOutput[2], null, 2)
+    );
+
+    await Promise.all([
+      save__walk,
+      save__walkProcessed,
+      save__walkProcessed,
+      save__walkProcessedItem
+    ]);
+  } catch (error) {
+    console.error(error);
+  }
+})();
--- package-scripts-add-examples.js
+++ package-scripts-add-examples.js
@@ -5,18 +5,6 @@ const cleanNodeModules = rimraf("node_modules");
 module.exports = {
   scripts: {
     default: "node index.js",
-    clear: {
-      default: {
-        description: "Deletes the `node_modules` directory",
-        script: series(cleanNodeModules)
-      }
-    },
-    test: {
-      default: {
-        description: "ava test",
-        script: "ava --verbose"
-      }
-    },
     examples: {
       default: {
         description: "run examples",
